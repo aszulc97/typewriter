c98ae575f7114fcd20723bba6f42023aa346b403
Agata Szulc <agataszulc97@gmail.com>
2022-02-10 10:47:15 +0100

    works fine with random timing

--- typewriter.js
+++ typewriter.js
@@ -5,16 +5,40 @@ let str = document.querySelector(".typewritten").textContent;
 window.addEventListener("load", start);
 
 function start() {
-  writeChar();
+  document.querySelector(".typewritten").textContent = "";
+  document.querySelector("#submit").addEventListener("click", function () {
+    writeChar();
+    document.querySelector("#submit").classList.add("hidden");
+  });
 }
 
 function writeChar() {
-  typewritingStr = typewritingStr + str.charAt(i);
-  i++;
-  if (i < str.length + 1) {
-    setTimeout(() => {
+  let times = [300, 600, 1000];
+  let time = Math.floor(Math.random() * 3);
+  setTimeout(() => {
+    if (i < str.length + 1) {
+      typewritingStr = typewritingStr + str.charAt(i);
       document.getElementById("typewriter").textContent = typewritingStr;
-      writeChar();
-    }, 200);
-  }
+      console.log(str.charAt(i));
+      console.log(times[time]);
+      if (str.charAt(i) === " ") {
+        document.getElementById("typespace").play();
+        console.log("space");
+        writeChar();
+      } else if (i === str.length) {
+        document.getElementById("typelast").play();
+        writeChar();
+      } else {
+        let rnd = Math.floor(Math.random() * 2);
+        if (rnd === 0) {
+          document.getElementById("typekey2").play();
+          writeChar();
+        } else if (rnd === 1) {
+          document.getElementById("typekey2").play();
+          writeChar();
+        }
+      }
+    }
+    i++;
+  }, times[time]);
 }
